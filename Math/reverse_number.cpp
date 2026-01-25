1264db992aa02a8edaee133e41961cfe424de50c
manigandan-d <manigandan220916@gmail.com>
2026-01-25 23:32:46 +0530

    Reverse a given number

---
+++ Math/reverse_number.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std; 
+
+int reverseNumber(int num);
+
+int main() {
+    int num; 
+    cin >> num; 
+
+    cout << reverseNumber(num) << endl; 
+
+    return 0;
+}
+
+int reverseNumber(int num) {
+    int reversed = 0;
+    int digit;
+
+    while (num != 0) {
+        digit = num % 10;
+        reversed = (reversed * 10) + digit;
+        num /= 10;
+    }
+
+    return reversed;
+}
