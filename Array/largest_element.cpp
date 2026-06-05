101c2be384caed52513e015de46f0d03bbd0b890
manigandan-d <manigandan220916@gmail.com>
2026-06-05 20:38:58 +0530

    Largest element in an array

---
+++ Array/largest_element.cpp
@@ -0,0 +1,20 @@
+#include <iostream> 
+
+using namespace std; 
+
+int main() {
+    int arr[] = {3, 7, 2, 9, 5};
+    int n = 5; 
+
+    int largest = arr[0];
+
+    for (int i=1; i<n; i++) {
+        if (arr[i] > largest) {
+            largest = arr[i];
+        }
+    }
+
+    cout << "Largest element: " << largest << endl; 
+
+    return 0;
+}
